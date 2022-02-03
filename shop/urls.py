2885fb0e040e25929e23e075032ffbb63c13b88c
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-02-03 17:34:36 +0300

    add to cart

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 
-from shop.views.templates_views import ProductView
+from shop.views.templates_views import (ProductView,
+                                        productDetailsView,
+                                        cartView, checkoutView
+                                        )
 
 app_name = "shop"
 
 urlpatterns = [
-    path('', ProductView, name="home")
+    path('', ProductView, name="home"),
+    path('product/<id>/details/',
+         productDetailsView,
+         name="productDetails"),
+    path('cart/', cartView, name="cart"),
+    path("checkout/",
+         checkoutView,
+         name="checkout")
 ]
--- shop/views/templates_views.py
+++ shop/views/templates_views.py
@@ -6,7 +6,31 @@ from shop.models import (Brand, Category, Order,
 
 def ProductView(request):
     productQuery = Product.objects.filter(available=True)
+    categoryQs = Category.objects.all()
     context = {
         "products": productQuery,
+        "categories": categoryQs
     }
     return render(request, "shop/product_page.html", context)
+
+
+def productDetailsView(request, id):
+    productObj = Product.objects.get(available=True, id=id)
+    context = {
+        "product": productObj
+    }
+    return render(request, "shop/product_details.html", context)
+
+
+def cartView(request):
+    context = {
+
+    }
+    return render(request, "shop/cart.html", context)
+
+
+def checkoutView(request):
+    context = {
+
+    }
+    return render(request, "shop/checkout.html", context)
